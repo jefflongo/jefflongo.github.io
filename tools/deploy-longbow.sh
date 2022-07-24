0ebcbfd6c3ce9dc1d6085766ad4f4e8e65a58602
jefflongo <jeff.a.longo@gmail.com>
2022-07-24 14:53:03 -0700

    make deploy script commit

--- tools/deploy-longbow.sh
+++ tools/deploy-longbow.sh
@@ -4,3 +4,7 @@ set -e
 readonly DEPLOY_DIR=$(dirname $(realpath $0))/../assets/files/longbow/release
 cp $1 ${DEPLOY_DIR}/dist.zip
 crc32 $1 > ${DEPLOY_DIR}/hash.txt
+
+git add ${DEPLOY_DIR}
+git commit -m "auto-pushing longbow deployment"
+git push origin jeff
