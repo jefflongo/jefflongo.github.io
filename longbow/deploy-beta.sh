80c7fa34290d793174ca161d9e4987602116c3bb
jefflongo <jeff.a.longo@gmail.com>
2022-12-22 00:23:43 -0800

    print beta in commit message

--- longbow/deploy-beta.sh
+++ longbow/deploy-beta.sh
@@ -6,5 +6,5 @@ cp $1 ${DEPLOY_DIR}/dist.zip
 crc32 $1 > ${DEPLOY_DIR}/hash.txt
 
 git add ${DEPLOY_DIR}
-git commit -m "auto-pushing longbow deployment"
+git commit -m "auto-pushing longbow beta deployment"
 git push origin jeff
