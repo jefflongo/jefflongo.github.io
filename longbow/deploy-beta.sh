17f7065a0b201658988e449c605edd2f14b5c6b0
jefflongo <jeff.a.longo@gmail.com>
2023-06-03 11:52:07 -0700

    add safety check for pushing to stable

--- longbow/deploy-beta.sh
+++ longbow/deploy-beta.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <path to distribution package>"
+    exit 1
+fi
+
 readonly DEPLOY_DIR=$(dirname $(realpath $0))/../assets/files/longbow/beta
 cp $1 ${DEPLOY_DIR}/dist.zip
 crc32 $1 > ${DEPLOY_DIR}/hash.txt
